f8c7186a02c44704b4a0b761495ef57fc8b93936
ALLUE <72491407+Cubeled@users.noreply.github.com>
2020-11-26 08:33:45 +0100

    Update and rename .gitignore to TP4.C

---
+++ TP4.C
@@ -0,0 +1,46 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+}
+
+void loop()
+{
+  int bp=digitalRead(8);
+  if (bp==0)
+  {
+  digitalWrite(3, HIGH);
+  digitalWrite(6, HIGH);
+  delay(1000); 
+  digitalWrite(3, LOW);
+  digitalWrite(6, LOW);
+  delay(1000);
+  }
+  else 
+  {
+  digitalWrite(2, HIGH);
+  digitalWrite(7, HIGH);
+  delay(45000);
+  digitalWrite(2, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(6, LOW);
+  delay(10000);
+  digitalWrite(3, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  delay(45000);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(10000);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(2, LOW);
+  }
+}
